4e13caacc9e6c1024843d755c22f2b3c3bbff9ab
Joanderson <joandersonsantossouza09@gmail.com>
2023-08-22 20:46:28 -0300

    My portifolio

---
+++ js/api_git.js
@@ -0,0 +1,63 @@
+const token_acess = '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';
+
+const requestOptions = {
+headers: {'Authorization': `Token ${token_acess}`}};
+
+fetch('https://api.github.com/users/eujoanderson/repos', requestOptions)
+  .then(response_date => response_date.json())
+  .then(data => {
+
+      data.forEach(reqData => {
+        const elemento = document.getElementById("box_container")
+
+        console.log(reqData)
+        console.log(reqData.name, reqData.language, reqData.visibility, reqData.id, reqData.html_url)
+
+        let status = reqData.language
+
+        if (status === null) {
+          status = "--";
+        }
+
+        //Date  
+        const date = new Date(reqData.created_at)
+        const dates = `${date.getDate().toString().padStart(2, '0')}/${date.getMonth().toString().padStart(2, '0')}/${date.getFullYear()}`
+        //Create element Dad
+        const element = document.createElement('div')
+        const span = document.createElement('span')
+
+        //Create element Child
+        const title = document.createElement('h4')
+        const paragraph = document.createElement('p')
+
+        //Create avatar
+        const avatar = document.createElement("a")
+        avatar.className = "bx bxl-github"
+        avatar.style = "font-size:50px;"
+        avatar.href = reqData.html_url
+        avatar.target = "__blank"
+        avatar.id = reqData.id
+        avatar.title = reqData.name
+
+        element.className = "box"
+        element.id = "box"
+
+        span.innerHTML = status
+        span.className = "languages"
+
+        title.innerHTML = reqData.name
+        paragraph.innerHTML = dates
+
+        //Element DAD
+        elemento.appendChild(element)
+        element.appendChild(avatar)
+        element.appendChild(title)
+        element.appendChild(paragraph)
+        paragraph.appendChild(span)
+        
+      })
+    })
+    .catch(error => {
+      console.error('Ocorreu um problema ao obter os detalhes do repositório:', error);
+});
+
---
+++ js/script.js
@@ -0,0 +1,35 @@
+const text = document.querySelector('.typewriter h4');
+const words = ['Desenvolvedor Web', 'Suporte Técnico'];
+let currentWord = 0;
+const wordPause = 4000;
+
+function typeWords() {
+  const word = words[currentWord];
+  let letterIndex = 0;
+  const typeInterval = setInterval(() => {
+
+    text.textContent += word[letterIndex];
+    letterIndex++;
+    if (letterIndex === word.length) {
+      clearInterval(typeInterval);
+      setTimeout(() => {
+        const deleteInterval = setInterval(() => {
+          text.textContent = text.textContent.slice(0, -1);
+          if (text.textContent === '') {
+            clearInterval(deleteInterval);
+
+            currentWord++;
+            if (currentWord === words.length) {
+              currentWord = 0;
+            }
+            setTimeout(typeWords, 0);
+          }
+        }, 100);
+      }, wordPause);
+    }
+  }, 100);
+}
+
+typeWords();
+
+
